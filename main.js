bbe161ef4368ed15cb99914cb7e266756cdf1505
Dylan Burkey <dylanburkey@outlook.com>
2019-11-06 12:21:43 -0500

    Add main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log(Date);
+
+
+function detectOrientation() {
+  if(window.innerHeight < window.innerWidth){
+    $('body').removeClass('portrait').addClass('landscape');
+  } else {
+    $('body').removeClass('landscape').addClass('portrait');
+  }
+}
